44936c9638108db2d7d059f3f0d13468af4712d3
tkchafin <tylerkchafin@gmail.com>
2024-06-19 09:40:46 +0100

    removed moon et al from repo

--- Moon_etal/data/MixedControl-07-12-23-V2A-fastqs/run_covid_analysis.sh
+++
@@ -1,3 +0,0 @@
-READ_DIR=ont/MixedControl-07-12-23-V2A-fastqs
-META=ont/MixedControl-07-12-23-V2A-fastqs/MixedControl-07-12-23-V2A.csv
-software/covid-analysis/run_pipeline.sh -p 07-12-23-V2A -f /fastq_pass -m  -o /output
